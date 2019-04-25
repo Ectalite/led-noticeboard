29b0cea2b6abccb7ef8c8ba35f54ba865e6f3c2a
Joseph Samela <josephsamela@gmail.com>
2019-04-24 21:22:01 -0400

    compatibility with hzeller rgb-matrix library

--- weatherstation.py
+++ weatherstation.py
@@ -4,118 +4,94 @@ import schedule
 import time
 import json
 import requests
-from rgbmatrix import Adafruit_RGBmatrix
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
-#Enter Location code found at: http://bulk.openweathermap.org/sample/city.list.json.gz
+# Enter Location code found at: http://bulk.openweathermap.org/sample/city.list.json.gz
 location = '2750065' #Nijkerk, NL
 #location = '4954380' #Waltham, MA
 
-#Include app id generated when you make you account at: http://openweathermap.org/api
+# Include app id generated when you make you account at: http://openweathermap.org/api
 appid = '26d71701f80249205ff46efa3570822f'
 
-#Define Matrix Dimensions
-matrix=Adafruit_RGBmatrix(32,1)
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'
+matrix = RGBMatrix(options = options)
+
+def drawimage(path, x, y):
+    image = Image.open(path).convert('RGB')
+    image.load()
+    matrix.SetImage(image, x, y)
 
 def job():
+    # Clear matrix
+    matrix.Clear()
+
+    # Pull fresh weather data
     try:
         response = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+location+'&mode=json&cnt=10&appid='+appid)
+
+        data = json.loads(response.text)
+        main = data['main']
+
+        #Get Current Conditions
+        temp = main['temp']
+        temp = ((temp-273.15)*(1.8)+32)
+        temp = int(round(temp))
+
+        weather = data['weather']
+        weather = weather[0]
+        icon = weather['icon']
+
+        Conditions = weather['id']
+
+        #Draw weather icon
+        if Conditions == 900:
+            drawimage('weathericons/' + 'tornado' + '.png', 9, 1)
+        elif Conditions == 901 or Conditions == 902:
+            drawimage('weathericons/' + 'hurricane' + '.png', 9, 1)
+        elif Conditions == 906 or Conditions == 611 or Conditions == 612:
+            drawimage('weathericons/' + 'hail' + '.png', 9, 1)
+        elif Conditions == 600 or Conditions == 601 or Conditions == 602:
+            drawimage('weathericons/' + 'snow' + '.png', 9, 1)
+        else: 
+            drawimage('weathericons/' + icon + '.png', 9, 1)
+
+        #Draw temperature
+        TempComponents = str(temp)
+        TempLength = len(TempComponents)
+
+        # Sets Temperature Color
+        if temp <= 32:
+            TempColor = 'b'
+        elif temp > 90:
+            TempColor = 'r'
+        else:
+            TempColor = 'w'
+
+        if TempLength == 1:
+            drawimage('numbericons/' + str(TempComponents[0]) + TempColor + '.png', 11, 16)
+            drawimage('numbericons/' + 'F' + TempColor + '.png', 17, 16)
+
+        if TempLength == 2:
+            drawimage('numbericons/' + str(TempComponents[0]) + TempColor + '.png', 7, 16)
+            drawimage('numbericons/' + str(TempComponents[1]) + TempColor + '.png', 13, 16)
+
+            drawimage('numbericons/' + 'F' + TempColor + '.png', 19, 16)
+
+        if TempLength == 3:
+            drawimage('numbericons/' + str(TempComponents[0]) + TempColor + '.png', 5, 16)
+            drawimage('numbericons/' + str(TempComponents[1]) + TempColor + '.png', 9, 16)
+            drawimage('numbericons/' + str(TempComponents[2]) + TempColor + '.png', 15, 16)
+            drawimage('numbericons/' + 'F' + TempColor + '.png', 21, 16)
+
+        print('Current Temp: '+str(temp)+' Icon Code: '+str(icon))
+
     except requests.exceptions.RequestException as e:
-        matrix.Clear()  
-        image = Image.open('weathericons/' + 'error' + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-    
-    data = json.loads(response.text)
-    main = data['main']
-
-    #Get Current Conditions
-    temp = main['temp']
-    temp = ((temp-273.15)*(1.8)+32)
-    temp = int(round(temp))
-
-    weather = data['weather']
-    weather = weather[0]
-    icon = weather['icon']
-
-    Conditions = weather['id']
-
-    #Draw weather icon
-    matrix.Clear()
-    
-    if Conditions == 900:
-        image = Image.open('weathericons/' + 'tornado' + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-    elif Conditions == 901 or Conditions == 902:
-        image = Image.open('weathericons/' + 'hurricane' + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-    elif Conditions == 906 or Conditions == 611 or Conditions == 612:
-        image = Image.open('weathericons/' + 'hail' + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-    elif Conditions == 600 or Conditions == 601 or Conditions == 602:
-        image = Image.open('weathericons/' + 'snow' + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-    else: 
-        image = Image.open('weathericons/' + icon + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 1)
-
-    #Draw temperature
-    TempComponents = str(temp)
-    TempLength = len(TempComponents)
-
-    # Sets Temperature Color
-    if temp <= 32:
-        TempColor = 'b'
-    elif temp > 90:
-        TempColor = 'r'
-    else:
-        TempColor = 'w'
-
-    if TempLength == 1:
-        image = Image.open('numbericons/' + str(TempComponents[0]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 11, 16)
-
-        image = Image.open('numbericons/' + 'F' + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 17, 16)
-
-    if TempLength == 2:
-        image = Image.open('numbericons/' + str(TempComponents[0]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 7, 16)
-
-        image = Image.open('numbericons/' + str(TempComponents[1]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 13, 16)
-
-        image = Image.open('numbericons/' + 'F' + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 19, 16)
-
-    if TempLength == 3:
-        image = Image.open('numbericons/' + str(TempComponents[0]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 5, 16)
-
-        image = Image.open('numbericons/' + str(TempComponents[1]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 9, 16)
-
-        image = Image.open('numbericons/' + str(TempComponents[2]) + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 15, 16)
-
-        image = Image.open('numbericons/' + 'F' + TempColor + '.png')
-        image.load()
-        matrix.SetImage(image.im.id, 21, 16)
-
-
-    print('Current Temp: '+str(temp)+' Icon Code: '+str(icon))
+        drawimage('weathericons/' + 'error' + '.png', 9, 1)
 
 job()
 schedule.every(5).minutes.do(job)
