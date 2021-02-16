306a2b17ed6170703ee1df4d9a9e4affffe5fc67
Dan Jones <61875664+danjcjones@users.noreply.github.com>
2021-02-16 20:10:57 +0000

    Noticeboard class to avoid global variable issues

--- noticeboard.py
+++ noticeboard.py
@@ -10,205 +10,219 @@ import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
+import sys
 
-# Weather settings held in file weather-data.json
-# Location code found at: http://bulk.openweathermap.org/sample/city.list.json.gz
-# Include app id generated when you make you account at: http://openweathermap.org/api
-with open('config.json') as json_file:
-    config = json.load(json_file)
-    weather_config = config['weather']
-    location = weather_config['location']
-    lat = weather_config['lat']
-    lon = weather_config['lon']
-    appid = weather_config['appid']
-    calendar_config = config['calendar']
-    calendarId = calendar_config['calendarId']
+class Noticeboard(object):
+    def process(self):
+        # Weather settings held in file weather-data.json
+        # Location code found at: http://bulk.openweathermap.org/sample/city.list.json.gz
+        # Include app id generated when you make you account at: http://openweathermap.org/api
+        with open('config.json') as json_file:
+            config = json.load(json_file)
+            weather_config = config['weather']
+            calendar_config = config['calendar']
 
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
-options.panel_type = 'FM6126A'
-matrix = RGBMatrix(options = options)
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.cols = 64
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = 'adafruit-hat'
+        options.panel_type = 'FM6126A'
+        self.matrix = RGBMatrix(options = options)
 
-font = graphics.Font()
-font.LoadFont('fonts/5x7.bdf')
+        self.font = graphics.Font()
+        self.font.LoadFont('fonts/5x7.bdf')
 
-smallFont = graphics.Font()
-smallFont.LoadFont("fonts/tom-thumb.bdf")
+        self.smallFont = graphics.Font()
+        self.smallFont.LoadFont("fonts/tom-thumb.bdf")
         
+        self.weatherJob(weather_config)
+        self.calendarJob(calendar_config)
+        schedule.every(5).minutes.do(self.weatherJob, config=weather_config)
+        schedule.every(1).minutes.do(self.calendarJob, config=calendar_config)    
 
-def drawimage(path, x, y):
-    image = Image.open(path).convert('RGB')
-    image.load()
-    matrix.SetImage(image, x, y)
-
-def weatherJob():
-    # Clear matrix
-    #matrix.Clear()
-
-    # Pull fresh weather data
-    try:
-        #weather_url = 'http://api.openweathermap.org/data/2.5/weather?id='+location+'&mode=json&units=metric&cnt=10&appid='+appid
-        weather_url = 'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&exclude=minutely,daily,alerts&appid={appid}'.format(
-            lat=lat, lon=lon, appid=appid)
-        
-        response = requests.get(weather_url)
-        data = json.loads(response.text)
-        print('Weather data: ' + str(data))
-        
-        #current = data['main']
-        current = data['current']
-        
-        #Get Current Conditions
-        temp = current['temp']
-        temp = int(round(temp))
-
-        #weather = data['weather']
-        weather = current['weather']
-        weather = weather[0]
-        icon = weather['icon']
-        
-        conditions = weather['id']
-        
-        #Get rain forecast
-        forecastHour = data['hourly'][0];
-        rainProb = forecastHour['pop']
-        timestamp = forecastHour['dt']
-        dt_object = datetime.datetime.fromtimestamp(timestamp)
-        print('Forecast time: ' + str(dt_object))
-        
-        #Draw weather icon
-        if conditions == 900:
-            icon = 'tornado'
-        elif conditions == 901 or conditions == 902:
-            icon = 'hurricane'
-        elif conditions == 906 or conditions == 611 or conditions == 612:
-            icon = 'hail'
-        elif conditions == 600 or conditions == 601 or conditions == 602:
-            icon = 'snow'
-
-        print('Current Temp: '+str(temp)+' Icon Code: '+str(icon))
-
-        #Draw temperature
-        # Sets Temperature Color
-        if temp >= 20:
-            tempColor = graphics.Color(255, 0, 0)
-        elif temp >= 15:
-            tempColor = graphics.Color(255, 128, 0)
-        elif temp >= 10:
-            tempColor = graphics.Color(255, 255, 0)
-        elif temp >= 5:
-            tempColor = graphics.Color(0, 255, 255)
-        else:
-            tempColor = graphics.Color(0, 0, 255)
-
-        tempFormatted = unicode(str(temp), 'utf-8') + unicode('°C', 'utf-8')
-        rainFormatted = str(int(rainProb*100)) + '%'
-        
-        drawimage('weathericons/' + icon + '.png', 0, 0)
-
-        # Clear current temp
-        image = Image.new('RGB', (24, 15))
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, 23, 14), fill=(0, 0, 0), outline=(0, 0, 0))
-        matrix.SetImage(image, 16, 0)
-
-        graphics.DrawText(matrix, font, 17, 7, tempColor, tempFormatted)
-        graphics.DrawText(matrix, font, 17, 14, tempColor, rainFormatted)
-
-    except requests.exceptions.RequestException as e:
-        drawimage('weathericons/' + 'error' + '.png', 0, 0)
+        try:
+            print("Press CTRL-C to stop")
+            while True:
+                schedule.run_pending()
+                self.timeJob()
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Exiting\n")
+            sys.exit(0)
 
 
-def timeJob():
-    #try:
-        color = graphics.Color(255, 0, 255)
+    def drawimage(self, path, x, y):
+        image = Image.open(path).convert('RGB')
+        image.load()
+        self.matrix.SetImage(image, x, y)
 
-        now = datetime.datetime.now()
-        onOff = (time.mktime(now.timetuple()) %2 == 0)
+    def weatherJob(self, config):
+        # Clear matrix
+        #matrix.Clear()
 
-        if onOff:
-            current_time = now.strftime("%H:%M")
-        else:
-            current_time = now.strftime("%H %M")
+        # Pull fresh weather data
+        try:
+            #location = config['location']
+            lat = config['lat']
+            lon = config['lon']
+            appid = config['appid']
+            
+            #weather_url = 'http://api.openweathermap.org/data/2.5/weather?id='+location+'&mode=json&units=metric&cnt=10&appid='+appid
+            weather_url = 'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&exclude=minutely,daily,alerts&appid={appid}'.format(
+                lat=lat, lon=lon, appid=appid)
+            
+            response = requests.get(weather_url)
+            data = json.loads(response.text)
+            print('Weather data: ' + str(data))
+            
+            #current = data['main']
+            current = data['current']
+            
+            #Get Current Conditions
+            temp = current['temp']
+            temp = int(round(temp))
 
-        current_date = now.strftime("%a%d")
+            #weather = data['weather']
+            weather = current['weather']
+            weather = weather[0]
+            icon = weather['icon']
+            
+            conditions = weather['id']
+            
+            #Get rain forecast
+            forecastHour = data['hourly'][0];
+            rainProb = forecastHour['pop']
+            timestamp = forecastHour['dt']
+            dt_object = datetime.datetime.fromtimestamp(timestamp)
+            print('Forecast time: ' + str(dt_object))
+            
+            #Draw weather icon
+            if conditions == 900:
+                icon = 'tornado'
+            elif conditions == 901 or conditions == 902:
+                icon = 'hurricane'
+            elif conditions == 906 or conditions == 611 or conditions == 612:
+                icon = 'hail'
+            elif conditions == 600 or conditions == 601 or conditions == 602:
+                icon = 'snow'
 
-        image = Image.new('RGB', (24, 15))
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, 23, 14), fill=(0, 0, 0), outline=(0, 0, 0))
-        matrix.SetImage(image, 40, 0)
+            print('Current Temp: '+str(temp)+' Icon Code: '+str(icon))
 
-        graphics.DrawText(matrix, font, 40, 7, color, current_time)
-        graphics.DrawText(matrix, font, 40, 14, color, current_date)
+            #Draw temperature
+            # Sets Temperature Color
+            if temp >= 20:
+                tempColor = graphics.Color(255, 0, 0)
+            elif temp >= 15:
+                tempColor = graphics.Color(255, 128, 0)
+            elif temp >= 10:
+                tempColor = graphics.Color(255, 255, 0)
+            elif temp >= 5:
+                tempColor = graphics.Color(0, 255, 255)
+            else:
+                tempColor = graphics.Color(0, 0, 255)
 
-    #except Exception as e:
-     #   print(e)
-     
+            tempFormatted = unicode(str(temp), 'utf-8') + unicode('°C', 'utf-8')
+            rainFormatted = str(int(rainProb*100)) + '%'
+            
+            self.drawimage('weathericons/' + icon + '.png', 0, 0)
 
-def calendarJob():
-    try:
-        
-        # If modifying these scopes, delete the file token.pickle.
-        #SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+            # Clear current temp
+            image = Image.new('RGB', (24, 15))
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((0, 0, 23, 14), fill=(0, 0, 0), outline=(0, 0, 0))
+            self.matrix.SetImage(image, 16, 0)
 
-        creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            print('invalid creds. run the quickstart')
+            graphics.DrawText(self.matrix, self.font, 17, 7, tempColor, tempFormatted)
+            graphics.DrawText(self.matrix, self.font, 17, 14, tempColor, rainFormatted)
 
-        service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        toDate = (datetime.date.today() + datetime.timedelta(days=1)).isoformat() + 'T00:00:00Z'
-        print('Getting the upcoming events')
-        print('From: '+now)
-        print('To: '+toDate)
-        events_result = service.events().list(calendarId=calendarId,
-                                            timeMin=now, timeMax=toDate, maxResults=3, singleEvents=True,
-                                            orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-
-        # Clear previous list
-        image = Image.new('RGB', (64, 17))
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, 63, 16), fill=(0, 0, 0), outline=(0, 0, 0))
-        matrix.SetImage(image, 0, 15)
-
-        color = graphics.Color(255, 255, 51)       
-        pos = 20
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            summary = event['summary']
-            print(start, summary)
-            graphics.DrawText(matrix, smallFont, 0, pos, color, summary)
-            pos+=6
+        except requests.exceptions.RequestException as e:
+            self.drawimage('weathericons/' + 'error' + '.png', 0, 0)
 
 
-    except Exception as e:
-        print(e)
+    def timeJob(self):
+        #try:
+            color = graphics.Color(255, 0, 255)
+
+            now = datetime.datetime.now()
+            onOff = (time.mktime(now.timetuple()) %2 == 0)
+
+            if onOff:
+                current_time = now.strftime("%H:%M")
+            else:
+                current_time = now.strftime("%H %M")
+
+            current_date = now.strftime("%a%d")
+
+            image = Image.new('RGB', (24, 15))
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((0, 0, 23, 14), fill=(0, 0, 0), outline=(0, 0, 0))
+            self.matrix.SetImage(image, 40, 0)
+
+            graphics.DrawText(self.matrix, self.font, 40, 7, color, current_time)
+            graphics.DrawText(self.matrix, self.font, 40, 14, color, current_date)
+
+        #except Exception as e:
+         #   print(e)
+         
+
+    def calendarJob(self, config):
+        try:
+            
+            # If modifying these scopes, delete the file token.pickle.
+            #SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+            creds = None
+            # The file token.pickle stores the user's access and refresh tokens, and is
+            # created automatically when the authorization flow completes for the first
+            # time.
+            if os.path.exists('token.pickle'):
+                with open('token.pickle', 'rb') as token:
+                    creds = pickle.load(token)
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+                print('invalid creds. run the quickstart')
+
+            service = build('calendar', 'v3', credentials=creds)
+
+            # Call the Calendar API
+            now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+            toDate = (datetime.date.today() + datetime.timedelta(days=1)).isoformat() + 'T00:00:00Z'
+            print('Getting the upcoming events')
+            print('From: '+now)
+            print('To: '+toDate)
+            calendarId = config['calendarId']
+            events_result = service.events().list(calendarId=calendarId,
+                                                timeMin=now, timeMax=toDate, maxResults=3, singleEvents=True,
+                                                orderBy='startTime').execute()
+            events = events_result.get('items', [])
+
+            if not events:
+                print('No upcoming events found.')
+
+            # Clear previous list
+            image = Image.new('RGB', (64, 17))
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((0, 0, 63, 16), fill=(0, 0, 0), outline=(0, 0, 0))
+            self.matrix.SetImage(image, 0, 15)
+
+            color = graphics.Color(255, 255, 51)       
+            pos = 20
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                summary = event['summary']
+                print(start, summary)
+                graphics.DrawText(self.matrix, self.smallFont, 0, pos, color, summary)
+                pos+=6
 
 
-weatherJob()
-calendarJob()
-schedule.every(5).minutes.do(weatherJob)
-schedule.every(1).minutes.do(calendarJob)
+        except Exception as e:
+            print(e)
 
-while True:
-    schedule.run_pending()
-    timeJob()
-    time.sleep(1)
+
+# Main function
+if __name__ == "__main__":
+    noticeboard = Noticeboard()
+    noticeboard.process()
